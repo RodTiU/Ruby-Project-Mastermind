10dd55aa2283c749dc87b19b1fb6e0b0b1c6cf03
Rodolfo <rudolfrce@gmail.com>
2022-03-14 20:15:51 -0700

    Add Solution to equal index, black dot implementation

--- game.rb
+++ game.rb
@@ -24,11 +24,36 @@ module Mastermind
           end
         end
       end
-      puts sequence
+      sequence
     end
 
     def self.get_blocks
       @@blocks
+    end
+  end
+
+  class Game
+    attr_reader :game
+
+    def initialize(code_length = $code_length, max_value = $max_value)
+      puts "Select the type of game (Maker = 1, Braker = 2)"
+      @game_selection = gets.chomp
+      if @game_selection != "1" && @game_selection != "2"
+        puts "Error typing. Select an available option"
+        restore_game()
+      end
+    end
+
+    def restore_game
+      initialize()
+    end
+
+    def start
+      if @game_selection == "1"
+        @game = GameMaker.new
+      elsif @game_selection == "2"
+        @game = GameBraker.new
+      end
     end
   end
 
@@ -69,15 +94,18 @@ module Mastermind
       loop do
         count += 1
         puts "#{count} attempt(s). Input your #{$code_length} digit code:"
-        code_input = gets.chomp
-        if CodeChecker.check_errors(code_input) != 0
+        $code_input = gets.chomp
+        if CodeChecker.check_errors($code_input) != 0
           puts "Wrong input. Repeat."
           count -= 1
           next
         end
-        code_input_array = code_input.split("")
-        Blocks.print_blocks_sequence(*code_input_array)
-        break puts "Congrats, you solve the code" if code_input == @code_to_break
+        code_input_array = $code_input.split("")
+        string_of_decoding = Blocks.print_blocks_sequence(*code_input_array) + "   |   " \
+          + GameDecode.decoding(@code_to_break, $code_input)
+        puts string_of_decoding
+        puts @code_to_break
+        break puts "Congrats, you solve the code" if $code_input == @code_to_break
         break puts "You lose" if count == 12
       end
     end
@@ -99,29 +127,20 @@ module Mastermind
     end
   end
 
-  class Game
-    def initialize(code_length = $code_length, max_value = $max_value)
-      puts "Select the type of game (Maker = 1, Braker = 2)"
-      @game_selection = gets.chomp
-      if @game_selection != "1" && @game_selection != "2"
-        puts "Error typing. Select an available option"
-        restore_game()
+  class GameDecode
+    def self.decoding(code_to_decode, code_get)
+      count_colors = 0
+      count_colors_and_position = 0
+      indexes_lock = Array.new
+      code_get.split("").each.with_index do |char, index|
+        if char == code_to_decode[index]
+          count_colors_and_position += 1
+          indexes_lock.push(index)
+        end
       end
-    end
-
-    def restore_game
-      initialize()
-    end
-
-    def start
-      if @game_selection == "1"
-        @game = GameMaker.new
-      elsif @game_selection == "2"
-        @game = GameBraker.new
-      end
+      count_color_sym = "○ " * count_colors
+      count_colors_and_position_sym = "● " * count_colors_and_position
+      count_colors_and_position_sym + count_color_sym
     end
   end
 end
-
-game = Mastermind::Game.new()
-game.start
